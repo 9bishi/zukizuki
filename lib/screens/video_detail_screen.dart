b9c21d1c8a6db2278ec6bfd58cbcd5daf3e7b779
9bishi <63241544+9bishi@users.noreply.github.com>
2024-11-28 13:09:16 +0800

    更新 video_detail_screen.dart

--- lib/screens/video_detail_screen.dart
+++ lib/screens/video_detail_screen.dart
@@ -1,60 +1,112 @@
+import 'dart:async';
+import '../utils/utils.dart';
+import 'package:chewie/src/center_play_button.dart';
+import 'package:chewie/src/chewie_player.dart';
+import 'package:chewie/src/chewie_progress_colors.dart';
+import 'package:chewie/src/helpers/utils.dart';
+import 'package:chewie/src/material/material_progress_bar.dart';
+import 'package:chewie/src/models/option_item.dart';
+import 'package:chewie/src/material/widgets/options_dialog.dart';
+import 'package:chewie/src/models/subtitle_model.dart';
+import 'package:chewie/src/notifiers/index.dart';
+import 'package:chewie/src/material/widgets/playback_speed_dialog.dart';
+import 'package:chewie/src/progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
-import '../providers/video_provider.dart';
-import '../utils/utils.dart';
-import 'package:chewie/chewie.dart';
 
-class VideoPlayerScreen extends StatefulWidget {
-  final String videoUrl; // 视频 URL (M3U8)
+class MaterialControls extends StatefulWidget {
+  const MaterialControls({
+    this.showPlayButton = true,
+    this.showControls = true,
+    Key? key,
+  }) : super(key: key);
 
-  const VideoPlayerScreen({Key? key, required this.videoUrl}) : super(key: key);
+  final bool showPlayButton;
+  final bool showControls;
 
   @override
-  _VideoPlayerScreenState createState() => _VideoPlayerScreenState();
+  State<StatefulWidget> createState() {
+    return _MaterialControlsState();
+  }
 }
 
-class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
-  late VideoPlayerController _videoPlayerController;
-  late ChewieController _chewieController;
+class _MaterialControlsState extends State<MaterialControls>
+    with SingleTickerProviderStateMixin {
+  late PlayerNotifier notifier;
+  late VideoPlayerValue _latestValue;
+  double? _latestVolume;
+  Timer? _hideTimer;
+  Timer? _initTimer;
+  late var _subtitlesPosition = Duration.zero;
+  bool _subtitleOn = false;
+  Timer? _showAfterExpandCollapseTimer;
+  bool _dragging = false;
+  bool _displayTapped = false;
+
+  final barHeight = 40.0;
+  final marginSize = 5.0;
+
+  late VideoPlayerController controller;
+  ChewieController? _chewieController;
+
+  // We know that _chewieController is set in didChangeDependencies
+  ChewieController get chewieController => _chewieController!;
 
   @override
   void initState() {
     super.initState();
-    _initializePlayer();
-  }
-
-  Future<void> _initializePlayer() async {
-    // 使用网络 URL 加载视频
-    _videoPlayerController = VideoPlayerController.network(widget.videoUrl);
-
-    await _videoPlayerController.initialize();
-
-    // 配置 ChewieController 来控制视频的播放
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController,
-      autoPlay: true, // 自动播放
-      looping: true,  // 循环播放
-      allowFullScreen: true, // 允许全屏播放
-      allowPlaybackSpeedMenu: true, // 允许选择播放速度
-    );
-
-    setState(() {}); // 刷新页面
+    notifier = Provider.of<PlayerNotifier>(context, listen: false);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!_videoPlayerController.value.isInitialized) {
-      return Scaffold(
-        body: Center(child: CircularProgressIndicator()), // 显示加载中的动画
-      );
+    if (_latestValue.hasError) {
+      return chewieController.errorBuilder?.call(
+            context,
+            chewieController.videoPlayerController.value.errorDescription!,
+          ) ??
+          const Center(
+            child: Icon(
+              Icons.error,
+              color: Colors.white,
+              size: 42,
+            ),
+          );
     }
 
-    return Scaffold(
-      appBar: AppBar(title: const Text("视频播放器")),
-      body: Center(
-        child: Chewie(
-          controller: _chewieController,
+    return MouseRegion(
+      onHover: (_) {
+        _cancelAndRestartTimer();
+      },
+      child: GestureDetector(
+        onTap: () => _cancelAndRestartTimer(),
+        child: AbsorbPointer(
+          absorbing: notifier.hideStuff,
+          child: Stack(
+            children: [
+              if (_latestValue.isBuffering)
+                const Center(
+                  child: CircularProgressIndicator(),
+                )
+              else
+                _buildHitArea(),
+              _buildActionBar(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  if (_subtitleOn)
+                    Transform.translate(
+                      offset: Offset(
+                          0.0, notifier.hideStuff ? barHeight * 0.8 : 0.0),
+                      child:
+                          _buildSubtitles(context, chewieController.subtitle!),
+                    ),
+                  _buildBottomBar(context),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -62,9 +114,67 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
 
   @override
   void dispose() {
+    _dispose();
     super.dispose();
-    _videoPlayerController.dispose();
-    _chewieController.dispose();
   }
-}
 
+  void _dispose() {
+    controller.removeListener(_updateState);
+    _hideTimer?.cancel();
+    _initTimer?.cancel();
+    _showAfterExpandCollapseTimer?.cancel();
+  }
+
+  @override
+  void didChangeDependencies() {
+    final _oldController = _chewieController;
+    _chewieController = ChewieController.of(context);
+    controller = chewieController.videoPlayerController;
+
+    if (_oldController != chewieController) {
+      _dispose();
+      _initialize();
+    }
+
+    super.didChangeDependencies();
+  }
+
+  Widget _buildActionBar() {
+    return Positioned(
+      top: 0,
+      right: 0,
+      child: SafeArea(
+        child: AnimatedOpacity(
+          opacity: notifier.hideStuff ? 0.0 : 1.0,
+          duration: const Duration(milliseconds: 250),
+          child: Row(
+            children: [
+              _buildSubtitleToggle(),
+              if (chewieController.showOptions) _buildOptionsButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOptionsButton() {
+    final options = <OptionItem>[
+      OptionItem(
+        onTap: () async {
+          Navigator.pop(context);
+          _onSpeedButtonTap();
+        },
+        iconData: Icons.speed,
+        title: chewieController.optionsTranslation?.playbackSpeedButtonText ??
+            'Playback speed',
+      )
+    ];
+
+    if (chewieController.subtitle != null &&
+        chewieController.subtitle!.isNotEmpty) {
+      options.add(
+        OptionItem(
+          onTap: () {
+            _onSubtitleTap();
+            Navigator.pop
