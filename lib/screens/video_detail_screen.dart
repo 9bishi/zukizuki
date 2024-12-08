f5b1f3647a3d580e443cc0f8f2daa0d52c0f95f8
9bishi <63241544+9bishi@users.noreply.github.com>
2024-12-08 16:55:09 +0800

    Update video_detail_screen.dart

--- lib/screens/video_detail_screen.dart
+++ lib/screens/video_detail_screen.dart
@@ -1,180 +1,126 @@
-import 'dart:async';
-import '../utils/utils.dart';
-import 'package:chewie/src/center_play_button.dart';
-import 'package:chewie/src/chewie_player.dart';
-import 'package:chewie/src/chewie_progress_colors.dart';
-import 'package:chewie/src/helpers/utils.dart';
-import 'package:chewie/src/material/material_progress_bar.dart';
-import 'package:chewie/src/models/option_item.dart';
-import 'package:chewie/src/material/widgets/options_dialog.dart';
-import 'package:chewie/src/models/subtitle_model.dart';
-import 'package:chewie/src/notifiers/index.dart';
-import 'package:chewie/src/material/widgets/playback_speed_dialog.dart';
-import 'package:chewie/src/progress_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
+import '../providers/video_provider.dart';
+import '../utils/utils.dart';
+class VideoDetailScreen extends StatefulWidget {
+  final int videoId;
 
-class MaterialControls extends StatefulWidget {
-  const MaterialControls({
-    this.showPlayButton = true,
-    this.showControls = true,
-    Key? key,
-  }) : super(key: key);
-
-  final bool showPlayButton;
-  final bool showControls;
+  const VideoDetailScreen({Key? key, required this.videoId}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() {
-    return _MaterialControlsState();
-  }
+  _VideoDetailScreenState createState() => _VideoDetailScreenState();
 }
 
-class _MaterialControlsState extends State<MaterialControls>
-    with SingleTickerProviderStateMixin {
-  late PlayerNotifier notifier;
-  late VideoPlayerValue _latestValue;
-  double? _latestVolume;
-  Timer? _hideTimer;
-  Timer? _initTimer;
-  late var _subtitlesPosition = Duration.zero;
-  bool _subtitleOn = false;
-  Timer? _showAfterExpandCollapseTimer;
-  bool _dragging = false;
-  bool _displayTapped = false;
-
-  final barHeight = 40.0;
-  final marginSize = 5.0;
-
-  late VideoPlayerController controller;
-  ChewieController? _chewieController;
-
-  // We know that _chewieController is set in didChangeDependencies
-  ChewieController get chewieController => _chewieController!;
+class _VideoDetailScreenState extends State<VideoDetailScreen> {
+  late VideoPlayerController _controller;
+  bool _isInitialized = false;
 
   @override
   void initState() {
     super.initState();
-    notifier = Provider.of<PlayerNotifier>(context, listen: false);
+    _initializeVideoPlayer();
+  }
+
+  Future<void> _initializeVideoPlayer() async {
+    try {
+      final videoProvider = Provider.of<VideoProvider>(context, listen: false);
+      final videoDetails = await videoProvider.fetchVideoDetails(widget.videoId);
+
+      if (videoDetails['resources'] != null && videoDetails['resources'].isNotEmpty) {
+        final resourceId = videoDetails['resources'][0]['id'];
+        final quality = "854x480_900k"; // 默认选择的分辨率
+
+        // 获取视频文件的 M3U8 URL
+        final m3u8Content = await videoProvider.fetchVideoFile(resourceId, quality);
+
+        if (m3u8Content.isNotEmpty) {
+          // 初始化视频播放器
+          _controller = VideoPlayerController.network(m3u8Content)
+            ..initialize().then((_) {
+              setState(() {
+                _isInitialized = true; // 更新初始化状态
+                _controller.play(); // 自动播放
+              });
+            });
+        } else {
+          throw Exception('Failed to fetch video file.');
+        }
+      } else {
+        throw Exception('No video resources available.');
+      }
+    } catch (error) {
+      print('Error initializing video player: $error');
+      setState(() {
+        _isInitialized = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_latestValue.hasError) {
-      return chewieController.errorBuilder?.call(
-            context,
-            chewieController.videoPlayerController.value.errorDescription!,
-          ) ??
-          const Center(
-            child: Icon(
-              Icons.error,
-              color: Colors.white,
-              size: 42,
-            ),
-          );
-    }
-
-    return MouseRegion(
-      onHover: (_) {
-        _cancelAndRestartTimer();
-      },
-      child: GestureDetector(
-        onTap: () => _cancelAndRestartTimer(),
-        child: AbsorbPointer(
-          absorbing: notifier.hideStuff,
-          child: Stack(
-            children: [
-              if (_latestValue.isBuffering)
-                const Center(
-                  child: CircularProgressIndicator(),
-                )
-              else
-                _buildHitArea(),
-              _buildActionBar(),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  if (_subtitleOn)
-                    Transform.translate(
-                      offset: Offset(
-                          0.0, notifier.hideStuff ? barHeight * 0.8 : 0.0),
-                      child:
-                          _buildSubtitles(context, chewieController.subtitle!),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Video Details'),
+      ),
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: Provider.of<VideoProvider>(context, listen: false)
+            .fetchVideoDetails(widget.videoId),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (snapshot.hasData) {
+            final video = snapshot.data!;
+            return ListView(
+              children: [
+                Image.network(
+                  video['cover'] ?? '',
+                  errorBuilder: (context, error, stackTrace) =>
+                      const Icon(Icons.error),
+                ),
+                ListTile(
+                  title: Text(video['title']),
+                  subtitle: Text(video['desc']),
+                ),
+                _isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        child: VideoPlayer(_controller),
+                      )
+                    : const Center(
+                        child: Text('Video not available or failed to load.')),
+                if (_isInitialized)
+                  IconButton(
+                    icon: Icon(
+                      _controller.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow,
                     ),
-                  _buildBottomBar(context),
-                ],
-              )
-            ],
-          ),
-        ),
+                    onPressed: () {
+                      setState(() {
+                        if (_controller.value.isPlaying) {
+                          _controller.pause();
+                        } else {
+                          _controller.play();
+                        }
+                      });
+                    },
+                  ),
+              ],
+            );
+          } else {
+            return const Center(child: Text('No video details available.'));
+          }
+        },
       ),
     );
   }
 
   @override
   void dispose() {
-    _dispose();
+    _controller.dispose();
     super.dispose();
   }
-
-  void _dispose() {
-    controller.removeListener(_updateState);
-    _hideTimer?.cancel();
-    _initTimer?.cancel();
-    _showAfterExpandCollapseTimer?.cancel();
-  }
-
-  @override
-  void didChangeDependencies() {
-    final _oldController = _chewieController;
-    _chewieController = ChewieController.of(context);
-    controller = chewieController.videoPlayerController;
-
-    if (_oldController != chewieController) {
-      _dispose();
-      _initialize();
-    }
-
-    super.didChangeDependencies();
-  }
-
-  Widget _buildActionBar() {
-    return Positioned(
-      top: 0,
-      right: 0,
-      child: SafeArea(
-        child: AnimatedOpacity(
-          opacity: notifier.hideStuff ? 0.0 : 1.0,
-          duration: const Duration(milliseconds: 250),
-          child: Row(
-            children: [
-              _buildSubtitleToggle(),
-              if (chewieController.showOptions) _buildOptionsButton(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildOptionsButton() {
-    final options = <OptionItem>[
-      OptionItem(
-        onTap: () async {
-          Navigator.pop(context);
-          _onSpeedButtonTap();
-        },
-        iconData: Icons.speed,
-        title: chewieController.optionsTranslation?.playbackSpeedButtonText ??
-            'Playback speed',
-      )
-    ];
-
-    if (chewieController.subtitle != null &&
-        chewieController.subtitle!.isNotEmpty) {
-      options.add(
-        OptionItem(
-          onTap: () {
-            _onSubtitleTap();
-            Navigator.pop
+}
